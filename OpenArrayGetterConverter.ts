2998cd42ede3652f64b42b5ec685bf602ca045fe
Steve Thompson <writetome51@gmail.com>
2018-10-18 17:36:36 -0600

    minor changes to each method

--- OpenArrayGetterConverter.ts
+++ OpenArrayGetterConverter.ts
@@ -1,4 +1,5 @@
 import { OpenArrayContainer } from '@writetome51/open-array-container/OpenArrayContainer';
+import { errorIfNotFunction } from 'basic-data-handling/errorIfNotFunction';
 
 
 export class OpenArrayGetterConverter extends OpenArrayContainer {
@@ -11,12 +12,17 @@ export class OpenArrayGetterConverter extends OpenArrayContainer {
 
 	// These functions don't modify the array.  What they return depends on the function.
 
+	// Does the same thing as Array.reduce(), but with a much better name.
 	toOne(reducingFunction): any {
+		errorIfNotFunction(reducingFunction);
 		return this.data.reduce(reducingFunction);
 	}
 
+
+	// Does the same thing as Array.map()
 	// Returns new array with each value in old array converted into something else.
 	each(mappingFunction): any[] {
+		errorIfNotFunction(reducingFunction);
 		return this.data.map(mappingFunction);
 	}
 
